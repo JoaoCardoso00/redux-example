3a2864ab0196537f6325c72b6b07a5424dc074dc
JoaoCardoso00 <jvcq13@gmail.com>
2022-07-02 01:41:02 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Provider } from "react-redux";
+import { Cart } from "./components/Cart";
+import { Catalog } from "./components/Catalog";
+import store from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Catalog />
+        <Cart />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
